7f5f85caf2d593ac9b837be35fc44f785dedefba
anis aichouba <aichoubaanis@gmail.com>
2022-07-01 14:50:53 +0200

    Ajoutez des fichiers projet.

---
+++ demosql4/Data/ResumeDbContext.cs
@@ -0,0 +1,15 @@
+﻿using demosql4.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace demosql4.Data
+{
+    public class ResumeDbContext:DbContext
+    {
+        public ResumeDbContext(DbContextOptions<ResumeDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Applicant> Applicants { get; set; }
+        public DbSet<Experience> Experiences { get; set; }
+    }
+}
---
+++ demosql4/Models/Experience.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace demosql4.Models
+{
+    public class Experience
+    {
+        public Experience()
+        {
+
+        }
+        [Key]
+        public int ExperienceId { get; set; }
+        [ForeignKey("Applicant")]
+        public int ApplicantId { get; set; }
+        public virtual Applicant Applicant { get; private set; }
+        public string CompanyName { get; set; }
+        public string Designation { get; set; }
+        [Range(1,25,ErrorMessage ="Years Must be  1 and 25")]
+        [Required]
+        public int YearsWorked { get; set; }
+        [NotMapped]
+        public bool IsDeleted { get; set; } = false;
+    }
+}
